6075d8c95344a8dd27fe301e2a859369acca8159
falak <khairkharu789@gmail.com>
2023-05-31 07:27:26 -0700

    FEAT: chap48

---
+++ Assignments/Assignment-43-48/app.js
@@ -0,0 +1,38 @@
+//questiojn no 3..............................................................................
+
+// function deleteRow(btn) {
+//     var row = btn.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+//   }
+
+//question no 4......................................................................................
+
+  // function changeImage() {
+  //   var img = document.getElementById("my-image");
+  //   img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTIlD-_kVCUKMkwrQYYq3CoMprsAlhw8LrK_CmNX0VL2uSZy7T&s";
+  // }
+  
+  // function resetImage() {
+  //   var img = document.getElementById("my-image");
+  //   img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc59ptKQgIUCoGDUzZ2eFA7Vu1caW9b18Zyq3T6nacQJD3ZQr9&s";
+  // }
+
+
+//question no 5------------------------------------------------------------------------------------------
+
+  var counter = 0;
+  var counterElement = document.getElementById("counter");
+  
+  function increaseCounter() {
+    counter++;
+    counterElement.innerText = counter;
+  }
+  
+  function decreaseCounter() {
+    counter--;
+    counterElement.innerText = counter;
+  }
+
+
+
+
